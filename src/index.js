e7e08c29f189642ffdd2cf1f707ab41d6ca2dbba
Sergey Durov <sergssd@yandex.com>
2020-10-02 17:19:37 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+let str
+  if(n < 0) {
+    str = String(-(n))
+  } else { 
+    str = String(n);
+  }
+  let arr = str.split('');
+
+  return +arr.reverse().join('')
 }
